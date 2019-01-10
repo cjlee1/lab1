4ec8281b8a45cebc1b77fcd084662dfae5545bc3
cjlee1 <cjlee@ualberta.ca>
2019-01-09 20:12:09 -0500

    delete2

--- script3.py
+++ script3.py
@@ -1,8 +1,12 @@
 import requests
 print(requests.__version__)
 
-r = requests.get("https://www.google.com")
-print(r.status_code)
-print(r.text)
+#r = requests.get("https://www.google.com")
+#print(r.status_code)
+#print(r.text)
 
-print(dir(r))
+#print(dir(r))
+
+a = requests.get("https://raw.githubusercontent.com/cjlee1/lab1/master/script3.py")
+
+print(a.text)
